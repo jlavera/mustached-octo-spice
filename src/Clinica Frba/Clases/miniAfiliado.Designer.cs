3200d98740b236ef9ff0bc47f28c426b5d867a02
Joaquín Hernán Lavera <jlavera92@gmail.com>
2013-12-02 23:42:54 -0300

    size minis

--- src/Clinica Frba/Clases/miniAfiliado.Designer.cs
+++ src/Clinica Frba/Clases/miniAfiliado.Designer.cs
@@ -32,7 +32,7 @@
             // 
             this.cbAfi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbAfi.FormattingEnabled = true;
-            this.cbAfi.Location = new System.Drawing.Point(12, 29);
+            this.cbAfi.Location = new System.Drawing.Point(24, 25);
             this.cbAfi.Name = "cbAfi";
             this.cbAfi.Size = new System.Drawing.Size(140, 21);
             this.cbAfi.TabIndex = 0;
@@ -40,7 +40,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 13);
+            this.label1.Location = new System.Drawing.Point(68, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 1;
@@ -48,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 56);
+            this.button1.Location = new System.Drawing.Point(24, 52);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(140, 23);
             this.button1.TabIndex = 1;
@@ -60,7 +60,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(164, 108);
+            this.ClientSize = new System.Drawing.Size(192, 92);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbAfi);
@@ -68,7 +68,7 @@
             this.MaximizeBox = false;
             this.Name = "miniAfiliado";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "miniAfiliado";
+            this.Text = "Seleccionar";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.miniAfiliado_Load);
             this.ResumeLayout(false);
