371b48a0ae4127e585a83dc781e64600798ff48f
Joaquín Hernán Lavera <jlavera92@gmail.com>
2013-12-02 23:05:49 -0300

    hehe

---
+++ src/Clinica Frba/Cancelar Atencion/Botonera.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Clinica_Frba.Cancelar_Atencion {
+    partial class Botonera {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.bAfiliado = new System.Windows.Forms.Button();
+            this.bProfesional = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bAfiliado
+            // 
+            this.bAfiliado.Location = new System.Drawing.Point(37, 12);
+            this.bAfiliado.Name = "bAfiliado";
+            this.bAfiliado.Size = new System.Drawing.Size(97, 23);
+            this.bAfiliado.TabIndex = 0;
+            this.bAfiliado.Text = "Afiliado";
+            this.bAfiliado.UseVisualStyleBackColor = true;
+            this.bAfiliado.Click += new System.EventHandler(this.bAfiliado_Click);
+            // 
+            // bProfesional
+            // 
+            this.bProfesional.Location = new System.Drawing.Point(165, 12);
+            this.bProfesional.Name = "bProfesional";
+            this.bProfesional.Size = new System.Drawing.Size(97, 23);
+            this.bProfesional.TabIndex = 1;
+            this.bProfesional.Text = "Profesional";
+            this.bProfesional.UseVisualStyleBackColor = true;
+            this.bProfesional.Click += new System.EventHandler(this.bProfesional_Click);
+            // 
+            // Botonera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 48);
+            this.Controls.Add(this.bProfesional);
+            this.Controls.Add(this.bAfiliado);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Botonera";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seleccione el rol";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bAfiliado;
+        private System.Windows.Forms.Button bProfesional;
+    }
+}
---
+++ src/Clinica Frba/Cancelar Atencion/Botonera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clinica_Frba.Cancelar_Atencion {
+    public partial class Botonera : Form {
+
+        public string rol;
+
+        public Botonera() {
+            InitializeComponent();
+        }
+
+        private void bAfiliado_Click(object sender, EventArgs e) {
+            rol = "Afiliado";
+            DialogResult = DialogResult.OK;
+        }
+
+        private void bProfesional_Click(object sender, EventArgs e) {
+            rol = "Profesional";
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
